0902a79fae710818fa5d42813448a046aa41a223
Rukhsar499 <rukshar.digitalwolf@gmail.com>
2025-10-14 19:13:34 +0530

    hii

---
+++ src/app/components/StickyFooter.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function StickyFooter() {
+  return (
+    <footer className="fixed bottom-0 left-0 w-full bg-[#1A7EBD] text-white py-4 shadow-md z-50 md:hidden">
+  <div className="container mx-auto px-6 flex justify-center items-center text-center space-x-2">
+    <span className="font-medium cursor-pointer">Login</span>
+    <span className="text-white">|</span>
+    <span className="font-medium cursor-pointer">Register</span>
+  </div>
+</footer>
+  );
+}
